ada9d4141032c76de196478e128e7f6462d4de76
zibnix <cfexit@protonmail.com>
2023-01-09 17:52:49 -0800

    cleanup sort test

--- sort_test.go
+++ sort_test.go
@@ -7,82 +7,58 @@ import (
 	"time"
 )
 
-var now time.Time = time.Now()
+var now = time.Now()
+
+var d1 = Data[int]{Timestamp: now}
+var d2 = Data[int]{Timestamp: now.Add(time.Second)}
+var d3 = Data[int]{Timestamp: now.Add(2 * time.Second)}
 
 func unsorted() []Data[int] {
-	return []Data[int]{
-		Data[int]{
-			Timestamp: now.Add(time.Second),
-		},
-		Data[int]{
-			Timestamp: now.Add(2 * time.Second),
-		},
-		Data[int]{
-			Timestamp: now,
-		},
-	}
+	return []Data[int]{d2, d3, d1}
 }
 
 func sorted() []Data[int] {
-	return []Data[int]{
-		Data[int]{
-			Timestamp: now,
-		},
-		Data[int]{
-			Timestamp: now.Add(time.Second),
-		},
-		Data[int]{
-			Timestamp: now.Add(2 * time.Second),
-		},
-	}
+	return []Data[int]{d1, d2, d3}
 }
 
 func reversed() []Data[int] {
-	return []Data[int]{
-		Data[int]{
-			Timestamp: now.Add(2 * time.Second),
-		},
-		Data[int]{
-			Timestamp: now.Add(time.Second),
-		},
-		Data[int]{
-			Timestamp: now,
-		},
-	}
+	return []Data[int]{d3, d2, d1}
 }
 
-func before(t *testing.T) {
-	if reflect.DeepEqual(unsorted(), sorted()) {
+func before(t *testing.T) (u, s, r []Data[int]) {
+	u = unsorted()
+	s = sorted()
+	r = reversed()
+
+	if reflect.DeepEqual(u, s) {
 		t.Fatal("unsorted and sorted []Data[int]'s were deep equal")
 	}
-	if reflect.DeepEqual(unsorted(), reversed()) {
+	if reflect.DeepEqual(u, r) {
 		t.Fatal("unsorted and reversed []Data[int]'s were deep equal")
 	}
-	if reflect.DeepEqual(sorted(), reversed()) {
+	if reflect.DeepEqual(s, r) {
 		t.Fatal("sorted and reversed []Data[int]'s were deep equal")
 	}
+
+	return
 }
 
 func TestSort(t *testing.T) {
-	before(t)
+	u, s, _ := before(t)
 
-	unsort := unsorted()
+	sort.Sort(ByTime[int](u))
 
-	sort.Sort(ByTime[int](unsort))
-
-	if !reflect.DeepEqual(sorted(), unsort) {
+	if !reflect.DeepEqual(s, u) {
 		t.Fatal("after sort.Sort: unsorted and sorted []Data[int]'s were not deep equal")
 	}
 }
 
 func TestReverse(t *testing.T) {
-	before(t)
+	u, _, r := before(t)
 
-	unsort := unsorted()
+	sort.Sort(sort.Reverse(ByTime[int](u)))
 
-	sort.Sort(sort.Reverse(ByTime[int](unsort)))
-
-	if !reflect.DeepEqual(reversed(), unsort) {
+	if !reflect.DeepEqual(r, u) {
 		t.Fatal("after sort.Reverse: unsorted and reversed []Data[int]'s were not deep equal")
 	}
 }
